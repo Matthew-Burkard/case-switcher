fbb65bb000b64f1ed31236c1bc19efbd8b88918d
Matthew Burkard <matthewburkard@protonmail.com>
2021-10-28 21:16:01 -0400

    feat: all case switchers working

--- src/caseswitcher/__init__.py
+++ src/caseswitcher/__init__.py
@@ -4,7 +4,7 @@ import re
 def to_camel(string: str) -> str:
     words = get_words(string)
     if len(words) > 0:
-        return words[0].lower() + "".join(map(lambda w: w.capitalize(), words[1:]))
+        return words[0].lower() + "".join(map(_capitalize, words[1:]))
     return string.lower()
 
 
@@ -14,11 +14,11 @@ def to_dot(string: str) -> str:
 
 def to_pascal(string: str) -> str:
     words = get_words(string)
-    return "".join(w.capitalize() for w in words)
+    return "".join(map(_capitalize, words))
 
 
 def to_kebab(string: str) -> str:
-    return "-".join(get_words(string))
+    return "-".join(map(lambda w: w.lower(), get_words(string)))
 
 
 def to_snake(string: str) -> str:
@@ -28,12 +28,12 @@ def to_snake(string: str) -> str:
 def to_title(string: str) -> str:
     words = get_words(string)
     if len(words) > 0:
-        return " ".join(w.capitalize() for w in words)
+        return " ".join(map(_capitalize, words))
     return string.capitalize()
 
 
 def get_words(string: str) -> list[str]:
-    words = [it for it in re.split(r"\b", string) if it and it not in ". -_"]
+    words = [it for it in re.split(r"\b|_", string) if it and it not in ". -_"]
     # Split on upper then lower: "oneTwo" -> ["one", "Two"]
     for word in words:
         camel_words = re.split(r"(?<=[a-z])(?=[A-Z])", word)
@@ -46,4 +46,9 @@ def get_words(string: str) -> list[str]:
         if len(camel_words) > 1:
             words.remove(word)
             words.extend(camel_words)
+    print(words)
     return words
+
+
+def _capitalize(word: str) -> str:
+    return word if word.isupper() else word.capitalize()
